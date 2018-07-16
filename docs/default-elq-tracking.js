99401c177fee2ebbf509ff3d0e96679fb54cda80
Brian Turner <Brian_turner@live.ca>
2018-07-16 14:36:09 -0400

    Add files via upload

---
+++ docs/default-elq-tracking.js
@@ -0,0 +1,14 @@
+
+var _elqQ = _elqQ || [];
+_elqQ.push(['elqSetSiteId', '2126376800']);
+_elqQ.push(['elqTrackPageView']);
+
+(function () {
+    function async_load() {
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
+        s.src = '//img.en25.com/i/elqCfg.min.js'; //Path to elqCfg.min.js
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    }
+    if (window.attachEvent) { window.attachEvent('onload', async_load); }
+    else { window.addEventListener('DOMContentLoaded', async_load, false); }
+})();
